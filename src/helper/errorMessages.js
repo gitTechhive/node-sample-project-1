29cc5fa405b8cf6bf7cb6338c3c2d32692c64665
vaik10 <vaidik@techhive.co.in>
2024-03-07 14:21:08 +0530

    Conflict resolved

--- src/helper/errorMessages.js
+++ src/helper/errorMessages.js
@@ -5,12 +5,8 @@ module.exports = {
     FIRST_NAME_IS_REQUIRED: "First Name is Required! ",
     LAST_NAME_IS_REQUIRED: "Last Name is Required! ",
     EMAIL_IS_REQUIRED: "Email is Required! ",
-<<<<<<< HEAD
     ENTER_VALID_EMAIL: "Enter Valid Email! ",
     MOBILE_NO_IS_REQUIRED: "Mobile no is Required! ",
-=======
-    MOBILE_NO_IS_REQUIRED: "Mobile no is required! ",
->>>>>>> origin/feature/captcha
     OTP_IS_REQUIRED: "Otp is Required! ",
     REQUEST_ID_IS_REQUIRED: "Request ID is Required! ",
     USER_IS_ALREAY_EXIST: "Email is Already Exist! ",
